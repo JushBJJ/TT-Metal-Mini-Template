1c574a7114c460d8d4fc0b3d9f67e757c759fc32
JushBJJ <36951064+JushBJJ@users.noreply.github.com>
2025-03-25 05:09:39 +1000

    Update template

--- example.cpp
+++ example.cpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+// SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -6,112 +6,111 @@
 #include <tt-metalium/device.hpp>
 #include <tt-metalium/bfloat16.hpp>
 
-/*
- * 1. Host writes data to buffer in DRAM
- * 2. dram_copy kernel on logical core {0, 0} BRISC copies data from buffer
- *      in step 1. to buffer in L1 and back to another buffer in DRAM
- * 3. Host reads from buffer written to in step 2.
- */
-
+using namespace tt;
 using namespace tt::tt_metal;
 
 int main(int argc, char** argv) {
-    if (getenv("TT_METAL_SLOW_DISPATCH_MODE") != nullptr) {
-        TT_THROW("Test not supported w/ slow dispatch, exiting");
-    }
+    /* Silicon accelerator setup */
+    IDevice* device = CreateDevice(0);
 
-    bool pass = true;
+    /* Setup program to execute along with its buffers and kernels to use */
+    CommandQueue& cq = device->command_queue();
+    Program program = CreateProgram();
+    constexpr CoreCoord core = {0, 0};
 
-    try {
-        /*
-         * Silicon accelerator setup
-         */
-        constexpr int device_id = 0;
-        IDevice* device = CreateDevice(device_id);
+    constexpr uint32_t single_tile_size = 2 * 1024;
+    tt_metal::InterleavedBufferConfig dram_config{
+        .device = device,
+        .size = single_tile_size,
+        .page_size = single_tile_size,
+        .buffer_type = tt_metal::BufferType::DRAM};
 
-        /*
-         * Setup program and command queue to execute along with its buffers and kernels to use
-         */
-        CommandQueue& cq = device->command_queue();
-        Program program = CreateProgram();
+    std::shared_ptr<tt::tt_metal::Buffer> src0_dram_buffer = CreateBuffer(dram_config);
+    std::shared_ptr<tt::tt_metal::Buffer> src1_dram_buffer = CreateBuffer(dram_config);
+    std::shared_ptr<tt::tt_metal::Buffer> dst_dram_buffer = CreateBuffer(dram_config);
 
-        constexpr CoreCoord core = {0, 0};
+    // Since all interleaved buffers have size == page_size, they are entirely contained in the first DRAM bank
+    uint32_t src0_bank_id = 0;
+    uint32_t src1_bank_id = 0;
+    uint32_t dst_bank_id = 0;
 
-        KernelHandle dram_copy_kernel_id = CreateKernel(
-            program,
-            "tt_metal/programming_examples/loopback/kernels/loopback_dram_copy.cpp",
-            core,
-            DataMovementConfig{.processor = DataMovementProcessor::RISCV_0, .noc = NOC::RISCV_0_default});
+    /* Use L1 circular buffers to set input and output buffers that the compute engine will use */
+    constexpr uint32_t src0_cb_index = CBIndex::c_0;
+    constexpr uint32_t num_input_tiles = 1;
+    CircularBufferConfig cb_src0_config =
+        CircularBufferConfig(num_input_tiles * single_tile_size, {{src0_cb_index, tt::DataFormat::Float16_b}})
+            .set_page_size(src0_cb_index, single_tile_size);
+    CBHandle cb_src0 = tt_metal::CreateCircularBuffer(program, core, cb_src0_config);
 
-        constexpr uint32_t single_tile_size = 2 * (32 * 32);
-        constexpr uint32_t num_tiles = 50;
-        constexpr uint32_t dram_buffer_size = single_tile_size * num_tiles;
+    constexpr uint32_t src1_cb_index = CBIndex::c_1;
+    CircularBufferConfig cb_src1_config =
+        CircularBufferConfig(num_input_tiles * single_tile_size, {{src1_cb_index, tt::DataFormat::Float16_b}})
+            .set_page_size(src1_cb_index, single_tile_size);
+    CBHandle cb_src1 = tt_metal::CreateCircularBuffer(program, core, cb_src1_config);
 
-        tt::tt_metal::InterleavedBufferConfig dram_config{
-            .device = device,
-            .size = dram_buffer_size,
-            .page_size = dram_buffer_size,
-            .buffer_type = tt::tt_metal::BufferType::DRAM};
-        tt::tt_metal::InterleavedBufferConfig l1_config{
-            .device = device,
-            .size = dram_buffer_size,
-            .page_size = dram_buffer_size,
-            .buffer_type = tt::tt_metal::BufferType::L1};
+    constexpr uint32_t output_cb_index = CBIndex::c_16;
+    constexpr uint32_t num_output_tiles = 1;
+    CircularBufferConfig cb_output_config =
+        CircularBufferConfig(num_output_tiles * single_tile_size, {{output_cb_index, tt::DataFormat::Float16_b}})
+            .set_page_size(output_cb_index, single_tile_size);
+    CBHandle cb_output = tt_metal::CreateCircularBuffer(program, core, cb_output_config);
 
-        auto l1_buffer = CreateBuffer(l1_config);
+    /* Specify data movement kernels for reading/writing data to/from DRAM */
+    KernelHandle binary_reader_kernel_id = CreateKernel(
+        program,
+        "./kernels/dataflow/reader_binary_1_tile.cpp",
+        core,
+        DataMovementConfig{.processor = DataMovementProcessor::RISCV_1, .noc = NOC::RISCV_1_default});
 
-        auto input_dram_buffer = CreateBuffer(dram_config);
-        const uint32_t input_dram_buffer_addr = input_dram_buffer->address();
+    KernelHandle unary_writer_kernel_id = CreateKernel(
+        program,
+        "./kernels/dataflow/writer_1_tile.cpp",
+        core,
+        DataMovementConfig{.processor = DataMovementProcessor::RISCV_0, .noc = NOC::RISCV_0_default});
 
-        auto output_dram_buffer = CreateBuffer(dram_config);
-        const uint32_t output_dram_buffer_addr = output_dram_buffer->address();
+    /* Set the parameters that the compute kernel will use */
+    std::vector<uint32_t> compute_kernel_args = {};
 
-        // Since all interleaved buffers have size == page_size, they are entirely contained in the first DRAM bank
-        const uint32_t input_bank_id = 0;
-        const uint32_t output_bank_id = 0;
+    /* Use the add_tiles operation in the compute kernel */
+    KernelHandle eltwise_binary_kernel_id = CreateKernel(
+        program,
+        "./kernels/compute/add_2_tiles.cpp",
+        core,
+        ComputeConfig{
+            .math_fidelity = MathFidelity::HiFi4,
+            .fp32_dest_acc_en = false,
+            .math_approx_mode = false,
+            .compile_args = compute_kernel_args,
+        });
 
-        /*
-         * Create input data and runtime arguments, then execute
-         */
-        std::vector<uint32_t> input_vec = create_random_vector_of_bfloat16(
-            dram_buffer_size, 100, std::chrono::system_clock::now().time_since_epoch().count());
-        EnqueueWriteBuffer(cq, input_dram_buffer, input_vec, false);
+    /* Create source data and write to DRAM */
+    std::vector<uint32_t> src0_vec;
+    std::vector<uint32_t> src1_vec;
+    src0_vec = create_constant_vector_of_bfloat16(single_tile_size, 14.0f);
+    src1_vec = create_constant_vector_of_bfloat16(single_tile_size, 8.0f);
 
-        const std::vector<uint32_t> runtime_args = {
-            l1_buffer->address(),
-            input_dram_buffer->address(),
-            input_bank_id,
-            output_dram_buffer->address(),
-            output_bank_id,
-            l1_buffer->size()};
+    EnqueueWriteBuffer(cq, src0_dram_buffer, src0_vec, false);
+    EnqueueWriteBuffer(cq, src1_dram_buffer, src1_vec, false);
 
-        SetRuntimeArgs(program, dram_copy_kernel_id, core, runtime_args);
+    /* Configure program and runtime kernel arguments, then execute */
+    SetRuntimeArgs(
+        program,
+        binary_reader_kernel_id,
+        core,
+        {src0_dram_buffer->address(), src1_dram_buffer->address(), src0_bank_id, src1_bank_id});
+    SetRuntimeArgs(program, eltwise_binary_kernel_id, core, {});
+    SetRuntimeArgs(program, unary_writer_kernel_id, core, {dst_dram_buffer->address(), dst_bank_id});
 
-        EnqueueProgram(cq, program, false);
-        Finish(cq);
+    EnqueueProgram(cq, program, false);
+    Finish(cq);
 
-        /*
-         * Validation & Teardown
-         */
-        std::vector<uint32_t> result_vec;
-        EnqueueReadBuffer(cq, output_dram_buffer, result_vec, true);
+    /* Read in result into a host vector */
+    std::vector<uint32_t> result_vec;
+    EnqueueReadBuffer(cq, dst_dram_buffer, result_vec, true);
 
-        pass &= input_vec == result_vec;
-
-        pass &= CloseDevice(device);
-
-    } catch (const std::exception& e) {
-        tt::log_error(tt::LogTest, "Test failed with exception!");
-        tt::log_error(tt::LogTest, "{}", e.what());
-
-        throw;
-    }
-
-    if (pass) {
-        tt::log_info(tt::LogTest, "Test Passed");
-    } else {
-        TT_THROW("Test Failed");
-    }
-
-    return 0;
+    printf("Result = %d\n", result_vec[0]);  // 22 = 1102070192
+    printf(
+        "Expected = %d\n",
+        pack_two_bfloat16_into_uint32(std::pair<bfloat16, bfloat16>(bfloat16(22.0f), bfloat16(22.0f))));
+    CloseDevice(device);
 }
